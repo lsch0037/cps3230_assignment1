43d0a6eec3076b41ead6009fda6a724100785ad3
Lukas Schimpf <lukas.schimpf@gmail.com>
2022-11-04 12:16:51 +0100

    Simple amazon search

--- src/test/java/com/lsch0037/cps3230/AppTest.java
+++ src/test/java/com/lsch0037/cps3230/AppTest.java
@@ -1,20 +1,27 @@
 package com.lsch0037.cps3230;
 
 import org.junit.jupiter.api.AfterEach;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 
 public class AppTest 
 {
     WebDriver driver;
 
+    //TODO: FIGURE OUT WHY THIS DOESNT WORK
     @BeforeEach
     public void setup(){
-        System.setProperty("webdriver.chrome.driver", "/home/schimpf/webtesting/chromedriver");
+        //FOR LINUX SYSTEM
+        //System.setProperty("webdriver.chrome.driver", "/home/schimpf/webtesting/chromedriver");
+
+        //FOR WINDOWS
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
         driver = new ChromeDriver();
     }
 
@@ -24,7 +31,31 @@ public class AppTest
     }
 
     @Test
-    public void testSimpleGoogleSearch(){
-        driver.get("https://www.google.com");
+    public void testSimpleGoogleSearch() throws Exception{
+
+        //TODO:REMOVE
+        setup();
+
+
+
+        //open amazon.de
+        driver.get("https://www.amazon.de");
+
+        //get the accept cookies button
+        WebElement acceptCookiesButton = driver.findElement(By.id("sp-cc-accept"));
+        WebElement searchBar = driver.findElement(By.id("twotabsearchtextbox"));
+        WebElement searchButton = driver.findElement(By.id("nav-search-submit-button"));
+
+        if(acceptCookiesButton != null)
+            acceptCookiesButton.sendKeys(Keys.ENTER);
+
+        searchBar.sendKeys("bluetooth speaker");
+
+        searchButton.sendKeys(Keys.ENTER);
+
+        Thread.sleep(1000);
+
+        //TODO:REMOVE
+        teardown();
     }
 }
