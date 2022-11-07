2e779698e7594b2e36e57ae8d32bb2b128a1f721
BuildTools <lukas.schimpf@gmail.com>
2022-11-07 18:38:37 +0100

    Function and unit test framework for task 1

---
+++ src/main/java/com/lsch0037/cps3230/ScreenScraper.java
@@ -0,0 +1,72 @@
+package com.lsch0037.cps3230;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+
+public class ScreenScraper 
+{
+
+    public static void main( String[] args ) throws Exception {
+        WebDriver driver = new ChromeDriver();
+        visitAmazon(driver);
+        searchAmazon(driver, "bluetooth Speaker");
+        scrapeAmazon(driver, 5);
+
+        driver.quit();
+    }
+
+    //nagivates to the amazon website
+    public static void visitAmazon(WebDriver driver){
+        //visit amazon.com website
+        driver.get("https://www.amazon.com");
+    }
+
+    //nagivates to the marketalertum website
+    public static void visitMarketAlert(WebDriver driver){
+        //visit marketalertum.com website
+        driver.get("https://www.marketalertum.com/");
+    }
+
+    public static void searchAmazon(WebDriver driver, String searchTerm){
+        //get the web elements
+        WebElement acceptCookiesButton = driver.findElement(By.id("sp-cc-accept"));
+        WebElement searchBar = driver.findElement(By.id("twotabsearchtextbox"));
+        WebElement searchButton = driver.findElement(By.id("nav-search-submit-button"));
+
+        //if cookies popup, click accept cookies
+        if(acceptCookiesButton != null)
+            acceptCookiesButton.sendKeys(Keys.ENTER);
+
+        //type in search term and press enter
+        searchBar.sendKeys(searchTerm);
+        searchButton.sendKeys(Keys.ENTER);
+
+    }
+
+    public static void scrapeAmazon(WebDriver driver, int amount){
+
+    }
+
+    public static void logInMarketAlert(WebDriver driver){
+
+    }
+
+    public static void viewAlerts(WebDriver driver){
+
+    }
+
+    public static void logOutMarketAlert(WebDriver driver){
+
+    }
+
+    public static void createAlert(WebDriver driver){
+
+    }
+
+    public static void deleteAlerts(WebDriver driver){
+
+    }
+}
---
+++ src/test/java/com/lsch0037/cps3230Test/ScreenScraperTest.java
@@ -0,0 +1,105 @@
+package com.lsch0037.cps3230Test;
+
+//TODO: CHANGE THIS TO ONLY IMPORT NECCESSARY THINGS
+import com.lsch0037.cps3230.ScreenScraper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+
+public class ScreenScraperTest 
+{
+    WebDriver driver;
+
+    @BeforeEach
+    public void setup(){
+
+        //TODO: AUTOMATICALLY FIGURE OUT THE SYSTEM AND SET THE CORRECT PROPERTY
+        /*String osname = System.getProperty("os.name");
+        if(osname.toLowerCase().contains("windows")){
+            //FOR LINUX SYSTEM
+            //System.setProperty("webdriver.chrome.driver", "/home/schimpf/webtesting/chromedriver");
+        }else if(osname.toLowerCase().contains("linux")){
+            //FOR WINDOWS
+            System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
+            driver = new ChromeDriver();
+        }
+        */
+
+        //FOR LINUX SYSTEM
+        //System.setProperty("webdriver.chrome.driver", "/home/schimpf/webtesting/chromedriver");
+
+        //FOR WINDOWS
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    public void teardown(){
+        driver.quit();
+    }
+
+
+    @Test
+    public void testVisitAmazon(){
+        //Visit amazon.com website
+        ScreenScraper.visitAmazon(this.driver);
+
+        //Verify that the title is the same title
+        String title = driver.getTitle();
+        assertEquals(title, "Amazon.com. Spend less. Smile more.");
+    }
+
+    @Test
+    public void testVisitMarketAlert(){
+        //Visit marketalertum.com website
+        ScreenScraper.visitMarketAlert(this.driver);
+
+        //Verify that the title is the same title
+        String title = driver.getTitle();
+        assertEquals(title, "Home Page - MarketAlertUM");
+    }
+
+    @Test
+    public void testSearchAmazon(){
+        assertTrue(false);
+    }
+
+    @Test
+    public void testScrapeAmazon(){
+        assertTrue(false);
+    }
+
+    @Test
+    public void testLogInMarketAlert(){
+        assertTrue(false);
+    }
+
+    @Test
+    public void testViewAlerts(){
+        assertTrue(false);
+    }
+
+    @Test
+    public void testLogOutMarketAlert(){
+        assertTrue(false);
+    }
+
+    @Test
+    public void testCreateAlert(){
+        assertTrue(false);
+    }
+
+    public void testDeleteAlerts(){
+        assertTrue(false);
+    }
+}
